e37144f5291cb1be9f722c08839accb829ce87c2
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-04 14:02:57 +0100

    Solution

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,6 +1,17 @@
 class PalindromeToTheDome
 
   def self.call(filename)
+    filename.gsub!(/\W+/, '')
+    if filename.length < 2
+      return true
+    end
+    if filename[0] != filename[-1]
+      return false
+    end
+    filename.slice!(0)
+    filename.slice!(-1)
+    return self.call(filename)
   end
 
 end
+
